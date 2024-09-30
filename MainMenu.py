db30aa4edbe0003274289740d8614f3a87f908bc
Kostya33 <123576484+Koljass@users.noreply.github.com>
2024-09-30 13:25:53 +0300

    Update MainMenu.py

--- MainMenu.py
+++ MainMenu.py
@@ -1,3 +1,4 @@
+from goto import with_goto
 import telebot
 from telebot import types
 admin_id = '1202757532'
@@ -10,34 +11,24 @@ inline_btn_5 = types.InlineKeyboardButton('вперед', callback_data=f'buk_{4
 btn3 = telebot.types.InlineKeyboardButton('выбрать', callback_data=f'buk_{1}')
 API_TOKEN = '7001627716:AAFtqdhmzsiymdpZnq4yOrkOkFDDb82VrFM'
 bot = telebot.TeleBot(API_TOKEN)
+#start_markup = telebot.types.InlineKeyboardMarkup()
+# Список товаров
+data = {}
 home_menu = {
-    1: "Популярные блюда",
-    2: "Салаты",
-    3: "Закуски",
-    4: "Супы",
-    5: "Пицца",
-    6: "Шашлык",
-    7: "Горячие блюда",
-    8: "Гарниры",
-    9: "Детское меню",
-    10: "Выпечка",
-    11: "Десерты",
-    12: "Напитки",
-    13: "Соусы"
+    1: "паста",
+    2: "бургер",
+    3: "суши",
+    4: "мой заказ"
 }
-salati = {
-    1: "Салат Азия",
-    2: "Салат Ташкентский",
-    3: "Салат Лаззат",
-    4: "Салат Ачик-чучук",
-    5: "",
-    6: "",
-    7: "",
-    8: "",
-    9: "",
-    10: "",
-    11: "",
+
+
+pasta_menu = {
+    1: {'name': 'паста 1','image': '15COOKING-PASTA-superJumbo-v2.jpg', 'price': 1},
+    2: {'name': 'паста 1','image': '13346e9c4ed98dff79263307baf0cb21-1100x825.jpg', 'price': 2},
+    3: {'name': 'паста 1','image': '6389174809.jpg', 'price': 3},
 }
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     global data
@@ -49,6 +40,9 @@ def start(message):
 
 @bot.message_handler(func=lambda message: message.text == "Посмотреть меню")
 def show_products(message):
+    bot.send_message(message.chat.id, '_', reply_markup=types.ReplyKeyboardRemove())
+
+
     markup = types.InlineKeyboardMarkup()
 
     for home_menu_id, home_menu_name in home_menu.items():
@@ -60,13 +54,78 @@ def show_products(message):
 
 
     @bot.callback_query_handler(func=lambda call: call.data.startswith('buy_'))
-    def show_products(call):
-        markup = types.InlineKeyboardMarkup()
-1
-        for home_menu_id, home_menu_name in home_menu.items():
-            button = types.InlineKeyboardButton(text=home_menu_name, callback_data=f'buy_{home_menu_id}')
-            markup.add(button)
+    def buy_product(call):
 
-        bot.send_message(message.chat.id, "Вот наше меню:", reply_markup=markup)
+        data = {}
+        home_menu_id = int(call.data.split('_')[1])
+        print(home_menu_id)
+        match home_menu_id:
+            case 1:
+
+                a_price = pasta_menu[1]['price']
+                image = open(pasta_menu[1]['image'], 'rb')
+                print(a_price)
+
+                start_markup.row(btn3)
+
+                start_markup.row(inline_btn_4, inline_btn_5)
+
+                bot.send_photo(call.message.chat.id, image, caption={a_price}, reply_markup=start_markup)
 
 
+                @bot.callback_query_handler(func=lambda popa: popa.data.startswith('buk_'))
+                def chenge_product(popa):
+                    kuda= int(popa.data.split('_')[1])
+                    match kuda:
+                        case 1:
+                            a_price = pasta_menu[1]['price']
+                            a_name = pasta_menu[1]['name']
+                            data = a_price,a_name
+                            print(data)
+                        case 4:
+                            print()
+
+                            a_price = pasta_menu[2]['price']
+                            image = open(pasta_menu[2]['image'], 'rb')
+                            print(a_price)
+
+                            start_markup.row(btn3)
+
+                            start_markup.row(inline_btn_3, inline_btn_4, inline_btn_5)
+
+                            bot.send_photo(call.message.chat.id, image, caption={a_price}, reply_markup=start_markup)
+
+                            @bot.callback_query_handler(func=lambda popa: popa.data.startswith('buk_'))
+                            def chenge_product(popa):
+                                kuda = int(popa.data.split('_')[1])
+                                match kuda:
+                                    case 1:
+                                        a_price = pasta_menu[1]['price']
+                                        a_name = pasta_menu[1]['name']
+                                        data = a_price, a_name
+                                        print(data)
+
+                        case 3:
+                            show_products(message)
+            case 2:
+                pass
+
+            case 3:
+                print("перни")
+
+"""
+product_id = int(call.data.split('_')[1])
+
+    product_name = home_menu[product_id]
+    match product_name:
+        case "Товар 1 - 100 ₽":
+            bot.send_message(admin_id, f"Пользователь @{call.from_user.username} купил: писю бобра")
+        case "Товар 2 - 200 ₽":
+            print('1')
+        case "Товар 3 - 300 ₽":
+            print("перни")
+    bot.send_message(admin_id, f"Пользователь @{call.from_user.username} купил: {product_name}")
+    bot.send_message(call.message.chat.id, f"Вы купили: {product_name}. Спасибо за покупку!")
+"""
+# Запуск бота
+bot.polling(none_stop=True)
