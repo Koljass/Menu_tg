26d9c02cd9bc947fd44cc77646a584f427519e62
Kostya33 <123576484+Koljass@users.noreply.github.com>
2024-11-25 10:31:51 +0300

    Update skelet.py

--- skelet.py
+++ skelet.py
@@ -1,1 +1,27 @@
-prtnt("fhcc")
+
+output = 0
+while 1 == 1:
+    match output:
+        case 0: 
+            output = int(input("2 корзина 1 меню "))
+        case 1:
+            output = int(input("3 вид 4 вид 5 вид 6 вид"))
+        case 2:
+            print('корзина')
+            
+        case 3:
+            print("вид 3")
+            print("пикча")
+            output = int(input("2 в корзину 1 назад"))
+        case 4:
+            print("вид 4")
+            print("пикча")
+            output = int(input("2 в корзину 1 назад"))
+        case 5:
+            print("вид 5")
+            print("пикча")
+            output = int(input("2 в корзину 1 назад"))
+        case 6:
+            print("вид 6")
+            print("пикча")
+            output = int(input("2 в корзину 1 назад"))
